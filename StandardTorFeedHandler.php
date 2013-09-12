32dd644d100876817e8e76ef6c04293da776e255
matuck <matuck@ubuntu>
2013-09-12 14:09:14 -0400

    update documentation and comments.

--- StandardTorFeedHandler.php
+++ StandardTorFeedHandler.php
@@ -1,11 +1,31 @@
 <?php
+/**
+ * This file is part of the TorFeed package.
+ *
+ * (c) Mitch Tuck <matuck@matuck.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace matuck\TorFeed\Handler;
 
 use matuck\TorFeed\Items;
 
+/**
+ * Standard handler for Torfeed
+ * Processes torrent feeds.
+ *
+ * @author Mitch Tuck<matuck@matuck.com>
+ */
 class StandardTorFeedHandler
 {
-    public function __construct($xml, &$items)
+    /**
+     * Constructor
+     * 
+     * @param SimpleXMLElement $xml
+     * @param array $items This is the return of items.
+     */
+    public function __construct($xml, &$items = array())
     {
         $items = array();
         foreach($xml->xpath('channel/item') as $node)
